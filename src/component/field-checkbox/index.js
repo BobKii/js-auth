12c75bb499e9ed844f6166038d67121a143dd82f
BobKii <two.heap2008@gmail.com>
2023-12-08 16:11:45 +0200

    08_12_23 commit_QA_1_2

--- src/component/field-checkbox/index.js
+++ src/component/field-checkbox/index.js
@@ -1,17 +1,6 @@
-class FieldPassword {
+class FieldCheckbox {
   static toggle = (target) => {
-    target.toggleAtribute("show");
-
-    const input = target.previousElementSibling;
-
-    const type = input.getAttribute("type");
-
-    if (type === "password") {
-      input.setAttribute("type", "text");
-    } else {
-      input.setAttribute("type", "password");
-    }
+    target.toggleAtribute("active");
   };
 }
-
-window.fieldPassword = FieldPassword;
+window.fieldCheckbox = FieldCheckbox;
